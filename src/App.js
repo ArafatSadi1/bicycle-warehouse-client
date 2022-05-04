4fd8f3fdd556839129510dae52768b0ead7e3a05
ArafatSadi1 <arafatsadi1@gmail.com>
2022-05-04 21:51:15 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline text-center">
+      <h2>Hello world</h2>
+    </div>
+  );
+}
+
+export default App;
